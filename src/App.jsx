1a156e150d11abdbf9286e1c9ce1a8ec911c5eed
zhoujie2001 <55381524+zhoujie2001@users.noreply.github.com>
2026-03-03 06:36:30 +0800

    Fix typo in video controls attribute

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ export default function App() {
                   muted
                   loop
                   playsInline
-                  control
+                  controls
                 />
               ))}
             </div>
